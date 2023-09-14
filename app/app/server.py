aa8d64616b4101c23313ba03b77b88e4bb39f69f
rohithreddykota <rohithreddykota@gmail.com>
2023-09-14 16:33:17 -0400

    fix server & add web-service

--- app/app/server.py
+++ app/app/server.py
@@ -1,9 +1,9 @@
 import http.server
 
-from prometheus import Counter, exposition
+from prometheus_client import Counter, exposition
 
 COUNTER_NAME = "request_count"
-REQUEST_COUNT = Counter(COUTNER_NAME, 'Count', ['endpoint'])
+REQUESTS_COUNT = Counter(COUNTER_NAME, 'Count', ['endpoint'])
 
 class RequestsHandler(exposition.MetricsHandler):
     
@@ -16,9 +16,9 @@ class RequestsHandler(exposition.MetricsHandler):
             return
         
         if self.path in ('/foo', '/bar'):
-            REQUEST_COUNT.labels(endpoint=self.path).inc()
+            REQUESTS_COUNT.labels(endpoint=self.path).inc()
             current_count = self._get_current_count()
-            self.response(200)
+            self.send_response(200)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(f'Current Count: [{self.path}] {current_count}'.encode())
@@ -26,6 +26,17 @@ class RequestsHandler(exposition.MetricsHandler):
         else:
             return super().do_GET()
 
+    def _get_current_count(self):
+        sample = [s for s in REQUESTS_COUNT.collect()[0].samples 
+                    if s.name == f'{COUNTER_NAME}_total' and s.labels['endpoint'] == self.path][0]
+        return sample.value
 
 
+if __name__ == '__main__':
+    for path in ('/foo', '/bar'):
+        REQUESTS_COUNT.labels(endpoint=path)
+
+    server_address = ('', 9191)
+    httpd = http.server.HTTPServer(server_address, RequestsHandler)
+    httpd.serve_forever()
 
